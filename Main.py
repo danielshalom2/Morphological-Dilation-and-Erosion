6484be47d0197af8d47ff0a517f270bfd67f6e9f
daniel <ddanielsshalom@gmail.com>
2020-08-25 10:36:07 +0300

    open image with plt. thanks to vlad :)

--- Main.py
+++ Main.py
@@ -6,18 +6,27 @@ from matplotlib import pyplot as plt
 
 
 def main():
-    # Reading the input image
-    img = cv2.imread('fingerprint2.jpg', 0)
+    # Reading the input image`
+    img = cv2.imread('fingerprint_1.png', 0)
+
     # Taking a matrix of size 5 as the kernel
-    kernel = np.ones((2, 2), np.uint8)
+    kernel = np.ones((3, 3), np.uint8)
     img_erosion = erosion(img, kernel)
     img_dilation = dilation(img, kernel)
+    plt.figure('Input')
+    plt.subplot(131)
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    plt.subplot(132)
+    plt.imshow(cv2.cvtColor(img_erosion, cv2.COLOR_BGR2RGB))
+    plt.subplot(133)
+    plt.imshow(cv2.cvtColor(img_dilation, cv2.COLOR_BGR2RGB))
+    plt.show()
 
-    cv2.imshow('Input', img)
-    cv2.imshow('Erosion', img_erosion)
-    cv2.imshow('Dilation', img_dilation)
+#    cv2.imshow('Input', img)
+#    cv2.imshow('Erosion', img_erosion)
+#    cv2.imshow('Dilation', img_dilation)
 
-    cv2.waitKey(0)
+#    cv2.waitKey(0)
 
 
 if __name__ == '__main__':
